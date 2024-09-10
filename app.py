a1dfbfdfb4ffbdad26fd302d9bbe7772013ac6e9
pedrolucas7i <145859069+pedrolucas7i@users.noreply.github.com>
2024-09-10 14:16:08 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,192 @@
+# PORTUGAL RADIATION PANEL (RADMAP)
+# CODE: Pedro Lucas
+# FILE: app.py
+
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+import fetch
+
+locals = []
+info_local = []
+
+# Load sensitive information
+load_dotenv('.env')
+host = os.getenv('MONGO_HOST')
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+database_name = os.getenv('MONGO_DATABASE')
+collection_name = os.getenv('MONGO_COLLECTION')
+
+# Conectar ao MongoDB
+client = MongoClient(f"mongodb://{host}/{database_name}")
+db = client[database_name]
+collection = db[collection_name]
+
+def update_data():
+    """Busca os dados do MongoDB e os converte para DataFrame"""
+    data = list(collection.find())  # Pega todos os documentos da coleção
+    for item in data:
+        item.pop('_id', None)  # Remove o campo _id que é gerado automaticamente
+    db_df = pd.DataFrame(data)
+    return db_df
+
+def get_place(place):
+    """Obtém o último registro de um local específico."""
+    global latest_data
+    data = latest_data[latest_data['place'] == place]
+    return data.iloc[-1] if not data.empty else None
+
+def get_all_from_place(place):
+    """Obtém todos os registros de um local específico."""
+    global latest_data
+    data = latest_data[latest_data['place'] == place]
+    df = pd.DataFrame(data, columns=['hour', 'place', 'value', 'latitude', 'longitude'])
+    return df
+
+def create_dataframes():
+    """Cria um DataFrame consolidado para os locais."""
+    global latest_data
+    data = []
+    for place in latest_data['place'].unique():
+        data.append(get_place(place))
+    df = pd.DataFrame(data, columns=['hour', 'place', 'value', 'latitude', 'longitude'])
+    return df
+
+def fetch_and_update_data():
+    """Busca novos dados a cada minuto e atualiza o DataFrame global."""
+    global latest_data
+    print("Atualizando dados...")
+    fetch.data_processing()
+    latest_data = update_data()
+    print("Dados atualizados!")
+
+# Inicializa os dados
+fetch_and_update_data()
+latest_data = update_data()
+
+# Configura o agendador para atualizar os dados a cada 5 minutos
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=fetch_and_update_data, trigger="interval", minutes=5)
+scheduler.start()
+
+selected_place = "Lisboa"
+
+# Cria a aplicação Dash
+app = dash.Dash(__name__, title="Radioactivity Dashboard")
+
+app.layout = html.Div([
+    html.Div(
+        id='info-title',
+        className='info-title',
+        children=[
+            html.H1("Radioactivity Dashboard"),
+            html.Div(
+                id='history',
+                className='history',
+                children=[
+                    dcc.Dropdown(
+                        id='title-dropdown',
+                        options=[],
+                        value=None,
+                        className='dropdown'
+                    ),
+                    html.Br(),
+                    dcc.Graph(
+                        id='history-graph',
+                    )
+            ])
+        ]
+    ),
+    dcc.Graph(
+        id='map',
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=60*5000,  # Update every 5 minutes
+        n_intervals=0
+    )
+])
+
+@app.callback(
+    [Output('title-dropdown', 'options'),
+     Output('title-dropdown', 'value')],
+    Input('title-dropdown', 'value')
+)
+def update_dropdown(selected_place):
+    """Atualiza as opções do dropdown com os locais disponíveis."""
+    dropdown_options = [{'label': place, 'value': place} for place in latest_data['place'].unique()]
+
+    # Define o valor inicial como Lisboa se não houver seleção
+    if selected_place is None:
+        initial_value = "Lisboa"
+    else:
+        initial_value = selected_place
+
+    return dropdown_options, initial_value
+
+@app.callback(
+    Output("history-graph", "figure"), 
+    Input("title-dropdown", "value"))
+def update_history_graph(place):
+    """Atualiza o gráfico histórico com base no local selecionado."""
+    selected_place = place if place is not None else selected_place
+    fig = px.line(
+        get_all_from_place(selected_place),
+        x="hour",
+        y="value",
+        color="place"
+    )
+    return fig
+
+@app.callback(
+    Output('map', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def update_map(n_intervals):
+    figure=go.Figure(
+        data=go.Scattermapbox(
+            lat=create_dataframes()['latitude'],
+            lon=create_dataframes()['longitude'],
+            mode='markers',
+            marker=dict(
+                size=(create_dataframes()['value'] / 9) + 5,  # Ajustar o tamanho dos marcadores
+                color=create_dataframes()['value'],
+                colorscale='Viridis',  # Escala de cores para os marcadores
+                colorbar=dict(
+                    title='Radioactivity'
+                )
+            ),
+            text=create_dataframes()['value'],
+            customdata=create_dataframes()[['hour', 'place', 'value']],
+            hovertemplate=(
+                "<b>Hora:</b> %{customdata[0]}<br>"
+                "<b>Local:</b> %{customdata[1]}<br>"
+                "<b>Valor:</b> %{customdata[2]} nSv/h<br>"
+                "<extra></extra>"
+            )
+        ),
+        layout=go.Layout(
+            mapbox=dict(
+                style="open-street-map",  # Estilo do mapa
+                center=dict(lat=create_dataframes()['latitude'].mean(), lon=create_dataframes()['longitude'].mean()),
+                zoom=5
+            ),
+            margin=dict(r=0, t=0, l=0, b=0)
+        )
+    )
+    return figure
+
+if __name__ == '__main__':
+    try:
+        app.run_server(debug=False, host='0.0.0.0')
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
---
+++ fetch.py
@@ -0,0 +1,112 @@
+# PORTUGAL RADIATION PANEL (RADMAP)
+# CODE: Pedro Lucas
+# FILE: fetch.py
+
+import requests
+from datetime import datetime, timedelta
+import json
+import pandas as pd
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import time
+import coordinates
+
+locals = []
+info_local = []
+
+# Load sensitive information
+load_dotenv('.env')
+host = os.getenv('MONGO_HOST')
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+database = os.getenv('MONGO_DATABASE')
+collection_name = os.getenv('MONGO_COLLECTION')
+
+# Conectar ao MongoDB
+client = MongoClient(f"mongodb://{host}/{database}")
+db = client[database]
+collection = db[collection_name]
+
+def fetch():
+    url = "https://radnet.apambiente.pt/ajax/dashboard/drawChart.php"
+
+    data_inicio = datetime.now()
+    data_fim = datetime.now()
+    data_inicio = data_fim - timedelta(hours=2)
+
+    data_inicio_str = data_inicio.strftime('%d/%m/%Y %H:%M')
+    data_fim_str = data_fim.strftime('%d/%m/%Y %H:%M')
+
+    params = {
+        'txtDataReport_start': data_inicio_str,
+        'txtDataReport_end': data_fim_str,
+        'jsonEstacoesList': '[1652, 7251168, 1303, 1307, 7593370, 1305, 1201, 7593374, 1311, 1302, 1404, 1310, 1552, 2711862, 1351, 1501, 7593377, 1306, 2711807, 1309, 1304, 1308, 7593380, 1252, 7251171, 1651]'
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            info = json.dumps(data)
+            return info, data_inicio_str, data_fim_str
+
+        except ValueError:
+            print("Erro ao converter resposta para JSON.")
+    else:
+        print(f"Erro na requisição: {response.status_code}")
+
+def store(hour, place, value, latitude, longitude):
+    # Verifica se o valor já existe no MongoDB
+    existing_data = collection.find_one({"value": value, "place": place, "hour": hour})
+    
+    if not existing_data:
+        # Cria o documento a ser inserido
+        radioactivity_data = {
+            "hour": hour,
+            "place": place,
+            "value": float(value),
+            "latitude": latitude,
+            "longitude": longitude
+        }
+        # Insere o documento no MongoDB
+        collection.insert_one(radioactivity_data)
+    else:
+        print("Radioatividade já registrada ✔")
+
+def data_processing():
+    info, hour_init, hour_end = fetch()
+    data = json.loads(info)
+    
+    # Save data to JSON file
+    with open('data/dados.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+    for i in range(len(data)):
+        locals.append(data[i]["label"])
+        info_local.append(data[i]["data"][int(len(data[i]["data"])) - 1][1])
+
+    print(hour_init)
+    print(hour_end)
+    print(locals)
+    print(info_local)
+    i = 0
+    for local in locals:
+        j = 0
+        # Retrieve coordinates from the dictionary
+        latitude_value, longitude_value = coordinates.coordinates[local]
+        value = info_local[i]
+        for place in coordinates.coordinates:
+            if place == local:
+                new_place_name = coordinates.indexed[j]
+                break
+            if not (latitude_value and longitude_value):
+                print(f"Coordinates not found for {local}")
+            j += 1
+        store(hour_end, new_place_name, value, latitude_value, longitude_value)
+        i += 1
+        time.sleep(0.2)
+        
+# Executa o processamento dos dados
+# data_processing()
